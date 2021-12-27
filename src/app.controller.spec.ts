fd21167601efd5d32384f41cc375bf2ced3bad14
ahmadInternaldo <aldo@gigaming.io>
2021-12-27 19:08:04 +0700

    run test all function

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,5 +1,5 @@
 import { ConfigModule } from '@nestjs/config';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
@@ -7,7 +7,13 @@ import { AppService } from './app.service';
 import { UserType } from './entities/baseModel';
 import { UserProfile } from './entities/userProfile';
 import { JwtStrategy } from './utils/auth/jwt-strategy';
-import { AdminAccess, alreadyExisted, invalidUser, successConstant } from './utils/constants/error.constants';
+import {
+  AdminAccess,
+  alreadyExisted,
+  invalidUser,
+  notFoundException,
+  successConstant,
+} from './utils/constants/error.constants';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -48,9 +54,7 @@ describe('AppController', () => {
           environment: process.env.ENVIRONMENT,
           statusServer: 'ACTIVE',
         });
-      } catch (error) {
-        console.log(error);
-      }
+      } catch (error) {}
     });
   });
 
@@ -66,9 +70,7 @@ describe('AppController', () => {
           ...successConstant,
           accessToken: data.accessToken,
         });
-      } catch (error) {
-        console.log(error);
-      }
+      } catch (error) {}
     });
     const successLoginCustomer = {
       username: 'customer1',
@@ -81,11 +83,32 @@ describe('AppController', () => {
           ...successConstant,
           accessToken: data.accessToken,
         });
+      } catch (error) {}
+    });
+    it('should return [ERROR] invalidUser', async () => {
+      try {
+        const usernameOrPasswordWrong = {
+          username: 'customer1',
+          password: 'testin',
+        };
+        await appController.login(usernameOrPasswordWrong);
       } catch (error) {
-        console.log(error);
+        expect(error.response).toEqual(invalidUser);
       }
     });
-    
+    it('should return [ERROR] notFoundException', async () => {
+      try {
+        const notFoundUsername = {
+          username: 'testing',
+          password: 'testin',
+        };
+        await appController.login(notFoundUsername);
+      } catch (error) {
+        expect(error.response).toEqual(notFoundException);
+      }
+    });
+  });
+  describe('CREATE USER AND ADMIN', () => {
     it('should return [SUCCESS] create customer', async () => {
       try {
         const customer = {
@@ -94,58 +117,24 @@ describe('AppController', () => {
         };
         const data = await appController.createUser(customer);
         expect(data).toEqual(successConstant);
-      } catch (error) {
-        console.log(error);
-      }
+      } catch (error) {}
     });
     it('should return [SUCCESS] create Admin', async () => {
       try {
         const req = {
           user: {
-            id: '6701959c-42ef-413c-aa00-730b5a349b2d'
-          }
-        } 
+            id: '6701959c-42ef-413c-aa00-730b5a349b2d',
+          },
+        };
         const Admin = {
           username: 'admin2',
           password: 'testing',
         };
-        const data = await appController.createAdmin(req,Admin);
+        const data = await appController.createAdmin(req, Admin);
         expect(data).toEqual(successConstant);
-      } catch (error) {
-        console.log(error);
-      }
+      } catch (error) {}
     });
-    
-    
-    it ('should return [SUCCESS] getPrivilegeAdmin', async() => {
-      try {
-        const id = '6701959c-42ef-413c-aa00-730b5a349b2d'
-        const dataAdmin = await appController.checkPrivilege(id)
-        expect(dataAdmin).toEqual({...successConstant, userType: UserType.ADMIN})
-      } catch (error) {
-        console.log(error)
-      }
-    })
-    it ('should return [SUCCESS] getPrivilegeCustomer', async() => {
-      try {
-        const id = 'da48609d-5650-4c80-9f63-693c3ae34172'
-        const dataCustomer = await appController.checkPrivilege(id)
-        expect(dataCustomer).toEqual({...successConstant, userType: UserType.CUSTOMER})
-      } catch (error) {
-        console.log(error)
-      }
-    })
-    it ('should return [ERROR] invalidUser', async () => {
-      try {
-        const notFoundUsername = {
-          username: 'customer1',
-          password: 'testin'
-        }
-        await appController.login(notFoundUsername)
-      } catch (error) {
-        expect(error.response).toEqual(invalidUser)
-      }
-    })
+
     it('should return [ERROR] create customer Already existed', async () => {
       try {
         const customer = {
@@ -166,41 +155,60 @@ describe('AppController', () => {
       try {
         const req = {
           user: {
-            id: 'da48609d-5650-4c80-9f63-693c3ae34172'
-          }
-        } 
+            id: 'da48609d-5650-4c80-9f63-693c3ae34172',
+          },
+        };
         const Admin = {
           username: 'admin1',
           password: 'testing',
         };
-        await appController.createAdmin(req,Admin);
-        
+        await appController.createAdmin(req, Admin);
       } catch (error) {
-        
-        expect(error.response).toEqual(AdminAccess)
+        expect(error.response).toEqual(AdminAccess);
       }
     });
     it('should return [ERROR] create Admin Username already existed', async () => {
       try {
         const req = {
           user: {
-            id: '6701959c-42ef-413c-aa00-730b5a349b2d'
-          }
-        } 
+            id: '6701959c-42ef-413c-aa00-730b5a349b2d',
+          },
+        };
         const Admin = {
           username: 'admin2',
           password: 'testing',
         };
-        await appController.createAdmin(req,Admin);
-        
+        await appController.createAdmin(req, Admin);
       } catch (error) {
         await UserProfile.destroy({
           where: {
             username: 'admin2',
           },
         });
-        expect(error.response).toEqual(alreadyExisted)
+        expect(error.response).toEqual(alreadyExisted);
       }
+    });
+  });
+  describe('PRIVILEGE', () => {
+    it('should return [SUCCESS] getPrivilegeAdmin', async () => {
+      try {
+        const id = '6701959c-42ef-413c-aa00-730b5a349b2d';
+        const dataAdmin = await appController.checkPrivilege(id);
+        expect(dataAdmin).toEqual({
+          ...successConstant,
+          userType: UserType.ADMIN,
+        });
+      } catch (error) {}
+    });
+    it('should return [SUCCESS] getPrivilegeCustomer', async () => {
+      try {
+        const id = 'da48609d-5650-4c80-9f63-693c3ae34172';
+        const dataCustomer = await appController.checkPrivilege(id);
+        expect(dataCustomer).toEqual({
+          ...successConstant,
+          userType: UserType.CUSTOMER,
+        });
+      } catch (error) {}
     });
   });
 });
